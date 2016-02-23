0c18ef157a4f376fc118d2adcdefb63c7c8b6e54
akincal <akinafl@gmail.com>
2016-02-23 09:40:37 +0200

    Desc Learn first commit
    image read is implemented

---
+++ image/image.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2015 Akin Caliskan.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OPEN_DL_METU_IMAGE_HPP
+#define OPEN_DL_METU_IMAGE_HPP
+
+
+#include <iostream>
+#include <vector>
+
+#include "openMVG/numeric/numeric.h"
+
+#include "Open_DL_METU/image/image_IO.hpp"
+#include "Open_DL_METU/image/image_container.hpp"
+#include "Open_DL_METU/image/image_converter.hpp"
+#include "Open_DL_METU/image/pixel_types.hpp"
+
+
+
+
+#endif /* OPEN_DL_METU_IMAGE_HPP */
